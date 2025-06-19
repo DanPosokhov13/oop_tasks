a8b341048180fbbf426ac39bc56eb112d0e8e5ef
Daniil Posokhov <147707574+Danposokhov@users.noreply.github.com>
2025-06-19 17:56:19 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -18,6 +18,31 @@ class Student:
         else:
             return 'Ошибка'
         
+    def for_average_grade(self):
+        self.all_grades = [self.grade for self.grades_list in [self.grades.values()] for self.grade in self.grades_list]
+        self.average = sum(self.all_grades) / len(self.all_grades) if self.all_grades else 0
+        return self.average
+
+    @classmethod
+    def check_grades(cls, other):
+        if not isinstance(other, Student):
+            return f'{other} не существует'
+        return other.for_average_grade()
+        
+    
+    def __lt__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() < oth
+    
+    def __eq__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() == oth
+    
+    def __gt__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() > oth
+    
+    
     def __str__(self):
         self.average_grade = mean(self.grades.values()) if len(self.grades) > 0 else 'нет'
         self.str_courses_in_progress = (', '.join(self.courses_in_progress)) if len(self.courses_in_progress) > 0 else 'нет'
@@ -37,6 +62,29 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+    
+    def for_average_grade(self):
+        self.all_grades = [self.grade for self.grades_list in [self.grades.values()] for self.grade in self.grades_list]
+        self.average = sum(self.all_grades) / len(self.all_grades) if self.all_grades else 0
+        return self.average
+    
+    @classmethod
+    def check_grades(cls, other):
+        if not isinstance(other, Lecturer):
+            return f'{other} не существует'
+        return other.for_average_grade()       
+    
+    def __lt__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() < oth
+    
+    def __eq__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() == oth
+    
+    def __gt__(self, other):
+        oth = self.check_grades(other)
+        return self.for_average_grade() > oth
 
     def __str__(self):
         self.average_grade = mean(self.grades.values()) if len(self.grades) > 0 else 'нет'
@@ -56,10 +104,14 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
  
 lecturer = Lecturer('Иван', 'Иванов')
+lecturer_1 = Lecturer('Илон', 'Маск')
+
 reviewer = Reviewer('Пётр', 'Петров')
 student = Student('Алёхина', 'Ольга', 'Ж')
+student_1 = Student('Вася', 'Акулов', 'М')
 
 student.grades.update({'Python': 5, 'Java': 10})
+student_1.grades.update({'Python': 7, 'Java': 8})
 lecturer.grades.update({'Python': 5, 'Java': 10})
 #student.finished_courses += ['Введение']
 student.courses_in_progress += ['Python', 'Java']
@@ -69,3 +121,12 @@ reviewer.courses_attached += ['Python', 'C++']
 print(student)
 print(lecturer)
 print(reviewer)
+
+print(student.for_average_grade())
+
+print(student_1 < student)  
+print(student_1 == student) 
+print(student_1 > student) 
+print(lecturer_1 < lecturer) 
+print(lecturer_1 == lecturer) 
+print(lecturer_1 > lecturer) 
